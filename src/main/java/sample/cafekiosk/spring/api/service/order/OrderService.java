c5e80ef0a6a66891c20628a00163cb5ef9adae87
seonghye0n <>
2024-04-07 13:47:17 +0900

    feat : Business Layer 테스트(2)

--- src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
+++ src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
@@ -2,6 +2,8 @@ package sample.cafekiosk.spring.api.service.order;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
@@ -25,10 +27,23 @@ public class OrderService {
 		//Product
 		List<Product> products = productRepository.findAllByProductNumberIn(productNumbers);
 
-		Order order = Order.create(products, registeredDateTime);
+		List<Product> duplicateProducts = findProductsBy(productNumbers,
+			products);
+
+		Order order = Order.create(duplicateProducts, registeredDateTime);
 
 		Order savedOrder = orderRepository.save(order);
 
 		return OrderResponse.of(savedOrder);
+	}
+
+	private static List<Product> findProductsBy(List<String> productNumbers, List<Product> products) {
+		Map<String, Product> productMap = products.stream()
+			.collect(Collectors.toMap(Product::getProductNumber, p -> p));
+
+		List<Product> duplicateProducts = productNumbers.stream()
+			.map(productMap::get)
+			.collect(Collectors.toList());
+		return duplicateProducts;
 	}
 }
--- src/test/java/sample/cafekiosk/spring/api/service/order/OrderServiceTest.java
+++ src/test/java/sample/cafekiosk/spring/api/service/order/OrderServiceTest.java
@@ -7,6 +7,7 @@ import static sample.cafekiosk.spring.domain.product.ProductType.*;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,8 @@ import org.springframework.test.context.ActiveProfiles;
 
 import sample.cafekiosk.spring.api.controller.order.request.OrderCreateRequest;
 import sample.cafekiosk.spring.api.service.order.response.OrderResponse;
+import sample.cafekiosk.spring.domain.order.OrderRepository;
+import sample.cafekiosk.spring.domain.orderproduct.OrderProductRepository;
 import sample.cafekiosk.spring.domain.product.Product;
 import sample.cafekiosk.spring.domain.product.ProductRepository;
 import sample.cafekiosk.spring.domain.product.ProductType;
@@ -28,6 +31,20 @@ class OrderServiceTest {
 
 	@Autowired
 	private OrderService orderService;
+
+	@Autowired
+	private OrderProductRepository orderProductRepository;
+
+	@Autowired
+	private OrderRepository orderRepository;
+
+	@AfterEach
+	void tearDown() {
+		//TODO: deleteAll <-> deleteAllInBatch 차이 학습하기
+		orderProductRepository.deleteAllInBatch();
+		productRepository.deleteAllInBatch();
+		orderRepository.deleteAllInBatch();
+	}
 
 	@DisplayName("주문번호 리스트를 받아 주문을 생성한다.")
 	@Test
@@ -57,6 +74,37 @@ class OrderServiceTest {
 			.containsExactlyInAnyOrder(
 				tuple("001", 1000),
 				tuple("002", 3000)
+			);
+	}
+
+	@DisplayName("중복되는 상품번호 리스트로 주문을 생성할 수 있다.")
+	@Test
+	void createOrderWithDuplicateProductNumbers() {
+		//given
+		Product product1 = createProduct(HANDMADE, "001", 1000);
+		Product product2 = createProduct(HANDMADE, "002", 3000);
+		Product product3 = createProduct(HANDMADE, "003", 5000);
+		productRepository.saveAll(List.of(product1, product2, product3));
+
+		OrderCreateRequest request = OrderCreateRequest.builder()
+			.productNumbers(List.of("001", "001"))
+			.build();
+
+		//when
+		LocalDateTime registeredDateTime = LocalDateTime.now();
+		OrderResponse response = orderService.createOrder(request, registeredDateTime);
+
+		//then
+		assertThat(response.getId()).isNotNull();
+		assertThat(response)
+			.extracting("registeredDateTime", "totalPrice")
+			.contains(registeredDateTime, 2000);
+
+		assertThat(response.getProducts()).hasSize(2)
+			.extracting("productNumber", "price")
+			.containsExactlyInAnyOrder(
+				tuple("001", 1000),
+				tuple("001", 1000)
 			);
 	}
 
