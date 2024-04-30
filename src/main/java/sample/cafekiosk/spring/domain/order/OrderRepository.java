d31a5b4b6d2293eb72361be713528fe5ee5cd66d
seonghye0n <seonghye0n@kakao.com>
2024-04-30 13:19:18 +0900

    Mockito로 Stubbing하기

--- src/main/java/sample/cafekiosk/spring/domain/order/OrderRepository.java
+++ src/main/java/sample/cafekiosk/spring/domain/order/OrderRepository.java
@@ -1,8 +1,15 @@
 package sample.cafekiosk.spring.domain.order;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
+	@Query("SELECT o from Order o WHERE o.registeredDateTime > :startDateTime AND o.registeredDateTime < :endDateTime"
+		+ " AND o.orderStatus = :orderStatus")
+	List<Order> findOrdersBy(LocalDateTime startDateTime, LocalDateTime endDateTime, OrderStatus orderStatus);
 }
--- src/test/java/sample/cafekiosk/CafekioskApplicationTests.java
+++ src/test/java/sample/cafekiosk/CafekioskApplicationTests.java
@@ -3,11 +3,13 @@ package sample.cafekiosk;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import sample.cafekiosk.spring.CafekioskApplication;
+
+@SpringBootTest(classes = CafekioskApplication.class)
 class CafekioskApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
